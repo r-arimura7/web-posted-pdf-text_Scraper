4d4f0e9641d2da0518cb47541cd3bed4c9f80ee5
tokyoevh <arimuu23@gmail.com>
2020-03-03 14:37:01 +0900

    Update mktCommentScraper.js
    ReportParse() execution process has been added (line 7).
    Deleted unnecessary test lines.

--- mktCommentScraper.js
+++ mktCommentScraper.js
@@ -1,14 +1,11 @@
 //@ts-check
-
-const reportFileName = "test";
 const ReportParse = require("./ReportParse");
-const TestResults = require("./TestFile");
+
 //const url = "http://www.am-one.co.jp/pdf/report/7377/200206_JSMarketDaily.pdf"; commented out for future inculusion of url argument in "results" function
 
-const main = () => {
-  const results = () => {
-    ReportParse();
-  };
+const main = async () => {
+  const results = await ReportParse();
+  console.log(results);
 };
-console.log(ReportParse);
+
 main();
