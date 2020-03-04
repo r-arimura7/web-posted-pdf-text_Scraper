1ef710ebd35e004e67703e44369da285735d978b
tokyoevh <arimuu23@gmail.com>
2020-03-04 16:32:08 +0900

    Bug_fix

--- ReportParse.js
+++ ReportParse.js
@@ -2,20 +2,21 @@ const fileSelect = require("fs");
 const pdf = require("pdf-parse");
 const crawler = require("crawler-request");
 
-const ReportParse = async function() {
-  await crawler(
-    "http://www.am-one.co.jp/pdf/report/7377/200206_JSMarketDaily.pdf"
-  ).then(response => {
-    const comment = response.text;
-    console.log(comment);
-    const headerTitle = comment.split("）");
-    const datestring = headerTitle[0].split("（");
-    const SplitComment = comment.split("【為替】");
-    const FXcomment = SplitComment[1].split("【リート・商品】");
-    console.log(`${datestring[1]} FX:${FXcomment[0]}`);
-    return FXcomment;
+function reportParse(url) {
+  return new Promise(resolve => {
+    crawler(url).then(response => {
+      const comment = response.text;
+      const headerTitle = comment.split("）");
+      const datestring = headerTitle[0].split("（");
+      const SplitComment = comment.split("【為替】");
+      const FXcomment = SplitComment[1].split("【リート・商品】");
+      const result = datestring[1].toString() + FXcomment[0].toString();
+      resolve(result);
+    });
   });
-};
+}
 
-console.log("Yop"); //test OUTPUT which works
-module.exports = ReportParse;
+module.exports.getFXComment = async function(url) {
+  const FXcomment = await reportParse(url);
+  return FXcomment;
+};
